f1d1617b463318b2019c64816e680fc0866c93f8
Yash Dubey <yashdubey1321998@gmail.com>
2024-12-09 10:33:17 +0100

    Update for yearly cost
    Code updates:
    1. Returns yearly cost from 2025 to 2050 in equal steps (linear interpolation)
    2. Stored master data in data folder.
    3. Added logic to separate SAF and DACCS

--- load_inputs.py
+++ load_inputs.py
@@ -6,29 +6,41 @@ import xlsxwriter
 def load_input_abatement_cost(file_path, tech):
     """Loads data on abatement costs from master excel sheet and formulates into a dataframe with min, 25%, median, 75%, and max values for each technology."""
 
-    # First row is headers and is skipped
-    input_abatement_cost = pd.read_excel(file_path, sheet_name='Standardization Results', skiprows=1, index_col=0)
-
     # Final cost column varies by technology
     if tech not in ['DACCS', 'SAF']: 
         raise ValueError('Technology not recognized. Please enter either DACCS or SAF.')
     
     if tech == 'DACCS':
-        col_name = "Fully Harmonized NET REMOVED COST (incl T&S"
+        cost_col = "Fully Harmonized NET REMOVED COST (incl T&S"
+        skiprows = 1
+        year_col = "Year of Assumption in Study"
     elif tech == 'SAF':
-        col_name = "Fully Harmonized"
+        cost_col = "Fully Harmonized"
+        skiprows = 0
+        year_col = "Year of Cost"
 
-    # Create a df to store the min, 25%, median, 75%, and max values for each technology
-    abatement_cost = input_abatement_cost[col_name].describe()
+    # First row is headers and is skipped
+    input_abatement_cost = pd.read_excel(file_path, sheet_name='Standardization Results', skiprows=skiprows, index_col=0)
 
-    return abatement_cost
+    input_abatement_cost_short = input_abatement_cost[input_abatement_cost[year_col] < 2031]
+    input_abatement_cost_long = input_abatement_cost[input_abatement_cost[year_col] == 2050] 
+
+    input_abatement_cost_short = input_abatement_cost_short[cost_col].describe()
+    input_abatement_cost_long = input_abatement_cost_long[cost_col].describe()
+
+    yearly_abatement_cost = {}
+    yearly_abatement_cost['25%'] = np.linspace(input_abatement_cost_short['25%'], input_abatement_cost_long['25%'], 25)
+    yearly_abatement_cost['75%'] = np.linspace(input_abatement_cost_short['75%'], input_abatement_cost_long['75%'], 25) 
+
+    return yearly_abatement_cost
 
 # Example Usage
-abatement_cost_daccs = load_input_abatement_cost("/Users/yash/Documents/GitHub/DACCSvDACCU/Master Standardisation DACCS.xlsx", tech='DACCS')
-abatement_cost_saf = load_input_abatement_cost("/Users/yash/Documents/GitHub/DACCSvDACCU/Master Standardisation_SAF.xlsx", tech='SAF')
+abatement_cost_saf = load_input_abatement_cost("data/Master Standardisation_SAF.xlsx", tech='SAF')
+abatement_cost_daccs = load_input_abatement_cost("data/Master Standardisation DACCS.xlsx", tech='DACCS')
 
 
 
+ 
 
 
     
